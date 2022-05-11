d94ba85860cd95abaae7eb52c11cfbfb8d621fc6
Kszanu <66957175+RafalChrzanowski@users.noreply.github.com>
2022-05-11 13:23:45 +0200

    Add files via upload

---
+++ Kolejka_i_stos.cpp
@@ -0,0 +1,12 @@
+﻿using namespace std;
+#include <iostream>
+#include <stack>
+
+
+int main()
+{
+    cout << "Hello World!\n";
+    stack <int> stos;
+    stos.push(123);
+    cout << stos.top();
+}
